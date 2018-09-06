df6dc512c4b665e79a36e2fecb9fcc758a4da7c4
Flynn <wphestiraid@gmail.com>
2018-09-06 23:17:38 +0900

    컴포넌트의 라이프사이클 메서드
    - 16.3 이후의 라이프 사이클

--- study/src/App.js
+++ study/src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
-import ScrollBox from './ScrollBox';
+import LifeCycleSample from './LifeCycleSample';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000'
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  };
+
   render() {
     return (
       <div>
-        <ScrollBox ref={ref => (this.scrollBox = ref)} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color} />
       </div>
     );
   }
